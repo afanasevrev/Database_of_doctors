3f107dec79ed3806bdd40f27a15bd24a3c96bee4
unknown <rewoly@mail.ru>
2024-05-17 08:05:36 +0300

    for student

--- Server/Server/src/main/java/com/example/Server/ServerController.java
+++ Server/Server/src/main/java/com/example/Server/ServerController.java
@@ -1,5 +1,10 @@
 package com.example.Server;
 
+import com.example.Server.db.Doctor;
+import com.example.Server.db.Patient;
+import com.example.Server.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 /**
@@ -16,5 +21,45 @@ public class ServerController {
     @GetMapping("/")
     private String getInfo() {
         return "База данных поликлиники";
+    }
+    /**
+     * Метод записывает в БД нового врача
+     * @param doctor
+     */
+    public synchronized void writeDoctor(Doctor doctor) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // Старт транзакции
+            transaction = session.beginTransaction();
+            // Добавим в БД клиента
+            session.persist(doctor);
+            // Коммит транзакции
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+    /**
+     * Метод записывает в БД нового пациента
+     * @param patient
+     */
+    public synchronized void writePatient(Patient patient) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // Старт транзакции
+            transaction = session.beginTransaction();
+            // Добавим в БД клиента
+            session.persist(patient);
+            // Коммит транзакции
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
 }
