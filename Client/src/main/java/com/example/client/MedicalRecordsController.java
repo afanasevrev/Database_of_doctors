fc827a96859e644791b6dcb52a2950fe9923a526
unknown <rewoly@mail.ru>
2024-05-18 16:07:46 +0300

    for student

---
+++ Client/src/main/java/com/example/client/MedicalRecordsController.java
@@ -0,0 +1,18 @@
+package com.example.client;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+/**
+ * Контроллер для докторов
+ */
+public class MedicalRecordsController {
+    @FXML
+    private TextField textFieldFirstName = new TextField();
+    @FXML
+    private TextField textFieldSpecialty = new TextField();
+    @FXML
+    private TextField textFieldOffice = new TextField();
+    @FXML
+    private TextField textFieldPhone = new TextField();
+
+}
