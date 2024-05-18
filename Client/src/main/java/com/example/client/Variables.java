5ff3f1f01735f608ca786fdc534c2e1de7d83d9e
unknown <rewoly@mail.ru>
2024-05-18 17:04:32 +0300

    for student

---
+++ Client/src/main/java/com/example/client/Variables.java
@@ -0,0 +1,8 @@
+package com.example.client;
+/**
+ * Статические переменные, здесь укажите ip - адрес сервера и порт сервера
+ */
+public class Variables {
+    public static String ip_server = "localhost";
+    public static String port_server = "8080";
+}
