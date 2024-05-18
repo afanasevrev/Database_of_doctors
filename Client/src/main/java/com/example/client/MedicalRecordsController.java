ef9e2fc49307c106c147ac449bb6f170de8d2c7d
unknown <rewoly@mail.ru>
2024-05-18 16:50:07 +0300

    for student

--- Client/src/main/java/com/example/client/MedicalRecordsController.java
+++ Client/src/main/java/com/example/client/MedicalRecordsController.java
@@ -46,5 +46,5 @@ public class MedicalRecordsController {
     private Button buttonCreateMedicalRecord = new Button();
 
     //------------------------------------------------------------//
-    
+
 }
