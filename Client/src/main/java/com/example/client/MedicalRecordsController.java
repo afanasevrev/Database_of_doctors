e3ebd2fc01e88af42b9accb8c5a25050e768fb81
unknown <rewoly@mail.ru>
2024-05-18 16:26:57 +0300

    for student

--- Client/src/main/java/com/example/client/MedicalRecordsController.java
+++ Client/src/main/java/com/example/client/MedicalRecordsController.java
@@ -1,6 +1,7 @@
 package com.example.client;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 /**
  * Контроллер для файла main.fxml
@@ -15,6 +16,23 @@ public class MedicalRecordsController {
     private TextField textFieldDoctorOffice = new TextField();
     @FXML
     private TextField textFieldDoctorPhone = new TextField();
-    
+    @FXML
+    private Button buttonCreateDoctor = new Button();
+
     //------------------------------------------------------------//
+    //Пациенты
+    @FXML
+    private TextField textFieldPatientFirstName = new TextField();
+    @FXML
+    private TextField textFieldPatientAddress = new TextField();
+    @FXML
+    private TextField textFieldPatientInsuranceNumber = new TextField();
+    @FXML
+    private TextField textFieldPatientSection = new TextField();
+    @FXML
+    private Button buttonCreatePatient = new Button();
+
+    //------------------------------------------------------------//
+    //Медицинские карты
+    
 }
